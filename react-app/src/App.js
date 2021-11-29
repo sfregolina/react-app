b5732ffb8b0a95eaa591a659258afab730bf1a44
Federica Sfregola <federica.sfregola@matchesfashion.com>
2021-11-29 18:01:35 +0000

    initial setup of the app

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="content">
+        <h2>App content</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
