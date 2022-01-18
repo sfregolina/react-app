2cba44cca77dc3eb61313027f2edf04265c552a2
Federica Sfregola <federica.sfregola@matchesfashion.com>
2022-01-18 14:01:50 +0000

    update style on header

--- react-app/src/components/Header.js
+++ react-app/src/components/Header.js
@@ -1,9 +1,14 @@
 const Header = () => {
   return (
-    <header className="header" style={{padding: "20px 0"}}>
+    <header className="header" style={{
+      padding: "20px 0 10px",
+      borderBottom: "1px solid #ccc",
+      marginBottom: "10px"
+    }}>
       <h2 style={{
         fontFamily: "Merriweather, serif",
-        fontSize: "28px"
+        fontSize: "18px",
+        textTransform: "uppercase"
         }}>My wishlist</h2>
     </header>
   );
