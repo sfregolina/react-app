bb8638263a823d1fb37374219578a03c7e1363dc
Federica Sfregola <federica.sfregola@matchesfashion.com>
2022-01-21 16:11:41 +0000

    use grid

--- react-app/src/components/ProductList.js
+++ react-app/src/components/ProductList.js
@@ -1,9 +1,9 @@
-const ProductList = ({ products, style }) => {
+const ProductList = ({ products }) => {
 
   return (
     <div className="product-list">
       {products.map(product => (
-        <div key={product.id} className="product" style={style}>
+        <div key={product.id} className="product">
           <img alt={product.name} src={product.image} style={{maxWidth: "100%"}}/>
           <h3>{product.designer}</h3>
           <p>{product.name}</p>
