8eb6010b689e4774712e93517d2bb2c08af7c007
Federica Sfregola <federica.sfregola@matchesfashion.com>
2022-01-21 17:12:15 +0000

    create facepaint function

---
+++ react-app/src/functions/useBreakpointValue.js
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import facepaint from 'facepaint'
+
+const breakpoints = [426, 769, 1441, 2561];
+
+const useBreakpointValue = facepaint(
+  breakpoints.map(bp => `@media (min-width: ${bp}px)`)
+);
+
+export default useBreakpointValue;
