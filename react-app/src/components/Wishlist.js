c0f84b341add46eefe527ee0caccdca2a264f3fe
Federica Sfregola <federica.sfregola@matchesfashion.com>
2022-01-31 16:45:35 +0000

    extract fetch in a custom hook

--- react-app/src/components/Wishlist.js
+++ react-app/src/components/Wishlist.js
@@ -1,25 +1,9 @@
-import { useState, useEffect } from 'react';
+import useFetch from './customHooks/useFetch';
 import Header from './Header';
 import ProductList from './ProductList';
 
 const Wishlist = () => {
-  const [products, setProducts] = useState(null);
-
-  useEffect(() => {
-    fetch('http://localhost:3000/products')
-      .then(res => {
-        if (!res.ok) {
-          throw Error('could not fetch the data for that recource');
-        }
-        return res.json();
-      })
-      .then(data => {
-        setProducts(data);
-      })
-      .catch(error => {
-        console.log(error.message);
-      });
-  }, []);
+  const { data: products } = useFetch('http://localhost:3000/products');
 
   return (
     <div className="wishlist-wrapper">
