dab13c29c1899420ed734b88aa94ea930639046c
Federica Sfregola <federica.sfregola@matchesfashion.com>
2022-01-31 16:14:17 +0000

    add cta button to product card

--- react-app/src/components/ProductCard.js
+++ react-app/src/components/ProductCard.js
@@ -22,12 +22,25 @@ const ProductCard = ({product}) => {
     }}>{ children }</p>
   )
 
+  const StyledCta = ({ children }) => (
+    <button css={{
+      fontSize: '12px',
+      backgroundColor: '#000',
+      color: '#FFF',
+      margin: '5px 0',
+      padding: '10px 20px',
+      border: 0,
+      textTransform: 'uppercase'
+    }}>{ children }</button>
+  )
+
   return (
     <StyledProduct key={product.id}>
       <img alt={product.name} src={product.image} style={{maxWidth: "100%"}}/>
       <StyledHeading3>{product.designer}</StyledHeading3>
       <StyledParagraph>{product.name}</StyledParagraph>
       <StyledParagraph>{product.price}</StyledParagraph>
+      <StyledCta>Add to bag</StyledCta>
     </StyledProduct>
   );
 }
